3a89521f975de625e9ca27d20acd7f2a5c5d3b93
Nathan Moes <nathanmoes7@gmail.com>
2023-05-26 17:16:25 -0700

    more complicated hello world

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-
+#![allow(unused)]
 
 use std::io;
 use rand::Rng;
+use std::io::{ Write, BufReader, BufRead, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+
 
 fn main() {
-    println!("Hello, world!");
+    println!("enter your name");
+    let mut name = String::new();
+    let greeting: &str = "Nice to meet you";
+    io::stdin().read_line(&mut name).expect("Didn't get input");
+    println!("Hello {}! {}", name.trim_end(), greeting);
 }
