8d1975bdfa5eba56d5aa6b7586494eaab686ba97
Nathan Moes <nathanmoes7@gmail.com>
2023-06-22 18:44:10 -0700

    e

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,44 @@ use rand::Rng;
 use std::io::{ Write, BufReader, BufRead, ErrorKind};
 use std::fs::File;
 use std::cmp::Ordering;
+use std::str::FromStr;
+use std::env;
 
 
 fn main() {
-    println!("enter your name");
-    let mut name = String::new();
-    let greeting: &str = "Nice to meet you.";
-    io::stdin().read_line(&mut name).expect("Didn't get input");
-    println!("Hello {}! {}", name.trim_end(), greeting);
+    let mut numbers = Vec::new();
+    for arg in env::args().skip(1){
+        numbers.push(u64::from_str(&arg)
+            .expect("error parsing argument"));
+    }
+    if numbers.len() == 0 {
+        eprintln!("Usage: gcd NUMBER ...");
+        std::process::exit(1);
+    }
+    let mut d = numbers[0];
+    for m in &numbers[1..]{
+        d = gcd(d, *m);
+    }
+    println!("The greatest common divisor of {:?} is {}", numbers, d);
+
+}
+
+
+fn gcd(mut n: u64, mut m: u64) -> u64{
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    return n
+}
+
+#[test]
+fn test_gcd(){
+    assert_eq!(gcd(14, 15), 1);
+    assert_eq!(gcd(2*3*5*11*17, 3*7*11*13*19), 3*11);
 }
